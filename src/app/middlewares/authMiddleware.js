dad874ec69c9ebbd72e31d38d7787741eafb0927
Valdir Coxev <valcox.russo@gmail.com>
2020-01-30 13:54:48 -0400

    Change auth middleware

--- src/app/middlewares/authMiddleware.js
+++ src/app/middlewares/authMiddleware.js
@@ -3,8 +3,8 @@ import { promisify } from 'util';
 import authConfig from '../../config/auth';
 
 export default async (request, response, next) => {
-  try {
-    const { authorization } = request.headers;
+  const { authorization } = request.headers;
+  if (typeof authorization !== 'undefined') {
     const [, token] = authorization.split(' ');
     if (!token) {
       return response.status(401).json({ message: 'Token not provided' });
@@ -12,7 +12,6 @@ export default async (request, response, next) => {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
     request.userId = decoded.id;
     return next();
-  } catch (error) {
-    return response.status(401).json({ message: 'Token invalid' });
   }
+  return response.status(500).json({ message: 'Not authorized' });
 };
