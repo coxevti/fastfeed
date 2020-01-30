ec17228627f53c3d718b36bd2b4ada42964d34b8
Valdir Coxev <valcox.russo@gmail.com>
2020-01-30 10:02:40 -0400

    Adding authentication middleware

---
+++ src/app/middlewares/authMiddleware.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+import authConfig from '../../config/auth';
+
+export default async (request, response, next) => {
+  try {
+    const { authorization } = request.headers;
+    const [, token] = authorization.split(' ');
+    if (!token) {
+      return response.status(401).json({ message: 'Token not provided' });
+    }
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+    response.userId = decoded.id;
+    return next();
+  } catch (error) {
+    return response.status(401).json({ message: 'Token invalid' });
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/authMiddleware';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
@@ -7,5 +9,7 @@ const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 export default routes;
