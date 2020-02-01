c547373f12d95ed37d8fba697077a29058274056
Valdir Coxev <valdircoxevdev@gmail.com>
2020-02-01 17:13:12 -0400

    Add the avatar in the login return

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,21 +1,27 @@
 import jwt from 'jsonwebtoken';
 
 import User from '../models/User';
+import File from '../models/Files';
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(request, response) {
     const { email, password } = request.body;
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        { model: File, as: 'avatar', attributes: ['id', 'path', 'url'] },
+      ],
+    });
     if (!user) {
       return response.status(400).json({ message: 'User does not exists.' });
     }
     if (!(await user.checkPassword(password))) {
       return response.status(401).json({ message: 'Authentication failed' });
     }
-    const { id, name } = user;
+    const { id, name, avatar } = user;
     return response.json({
-      user: { id, name, email },
+      user: { id, name, email, avatar },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
       }),
